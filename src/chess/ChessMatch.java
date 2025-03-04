53c5f9aa0ff60379f6e12178c6d3197b89e657c1
hvgofernandes <hvgofernandes@gmail.com>
2025-03-04 11:53:23 -0300

    feat: Piece move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -63,7 +63,8 @@ public class ChessMatch {
     }
 
     private Piece makeMove(Position source, Position target) {
-        Piece p = board.removePiece(source);
+        ChessPiece p = (ChessPiece) board.removePiece(source);
+        p.increaseMoveCount();
         Piece capturePiece = board.removePiece(target);
         board.placePiece(p, target);
         if (capturePiece != null) {
@@ -74,7 +75,8 @@ public class ChessMatch {
     }
 
     private void undoMove(Position source, Position target, Piece capturePiece) {
-        Piece p = board.removePiece(target);
+        ChessPiece p = (ChessPiece) board.removePiece(target);
+        p.decreaseMoveCount();
         board.placePiece(p, source);
         if (capturePiece != null) {
             board.placePiece(capturePiece, target);
